8bce9175c0a2fa2b0ccca55ef3f1791a0ea3aff1
LloydIrving01 <brnrdvl709@gmail.com>
2023-02-28 22:04:51 -0800

    show route connect

--- controllers/places.js
+++ controllers/places.js
@@ -28,4 +28,18 @@ router.post('/', (req, res) => {
   res.redirect('/places')
 })
 
+// Show
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if(!places[id]){
+    res.render('error404')
+  }
+  else {
+  res.render('/places/show', {place: places[id]})
+  }
+})
+
 module.exports = router
