983cdbe5bad89bddd7cf60576237eab8973f2d1e
LloydIrving01 <brnrdvl709@gmail.com>
2023-03-02 19:09:35 -0800

    delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -1,15 +1,16 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
 
+// New
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+
 // Index
 router.get('/', (req,res) => {
     res.render('places/index', {places})
 })
 
-// New
-router.get('/new', (req, res) => {
-  res.render('places/new')
-})
 
 // Post /places
 router.post('/', (req, res) => {
@@ -38,7 +39,21 @@ router.get('/:id', (req, res) => {
     res.render('error404')
   }
   else {
-  res.render('/places/show', {place: places[id]})
+  res.render('/places/show', {place: places[id], id})
+  }
+})
+
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(id, 1)
+    res.redirect('/places')
   }
 })
 
